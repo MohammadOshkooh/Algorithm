75bfbdb783b6826881a1fa20455bed80b9ee8e8f
mohammadoshkooh <mohammaadoshkooh@gmail.com>
2023-08-18 14:52:05 +0330

    caesar_cipher.py

---
+++ caesar_cipher.py
@@ -0,0 +1,19 @@
+from string import ascii_letters
+
+
+def encrypt(string, shift):
+    alphabet = ascii_letters
+    result = ""
+
+    for char in string:
+        if char not in alphabet:
+            result += char
+        else:
+            new_key = (alphabet.index(char) + shift) % len(alphabet)
+            result += alphabet[new_key]
+    return result
+
+
+def decrypt(string, shift):
+    shift *= -1
+    return encrypt(string, shift)
